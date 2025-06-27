76ffd196bbaf2904806e9176915df2ced9903209
charan kumar <charankumarbijjaram@gmail.com>
2025-06-27 10:15:52 +0530

    first commit

---
+++ block.js
@@ -0,0 +1,55 @@
+const { GENESIS_DATA,MINE_RATE} =require('./config');
+const cryptoHash = require('./crypto-hash');
+
+class Block {
+  constructor({ timestamp, lastHash, hash, data , nonce, difficulty }) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data =  data;
+    this.nonce = nonce;
+  }
+
+  static genesis(){
+    return new this(GENESIS_DATA);
+  }
+  
+
+  static mineBlock({lastBlock,data}){
+    let hash, timestamp;
+    let lastHash = lastBlock.hash;
+    let { difficulty } = lastBlock;
+    let nonce = 0;
+
+   do{
+      nonce++;
+      timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp});
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+  
+    return new this({
+      timestamp,
+      lastHash,
+      hash,
+      data,
+      nonce,
+      difficulty
+    });
+  }
+
+  static adjustDifficulty({originalBlock, timestamp}) {
+    const { difficulty } = originalBlock;
+
+    if (difficulty < 1) {
+      return 1;
+    }
+
+    if(( difficulty - originalBlock.timestamp)> MINE_RATE) {
+      return difficulty - 1;
+    }
+    return difficulty + 1;
+  }
+
+}
+module.exports = Block;
