3e45886750a7bffa9cfaa46c2185e473b634fa0b
charan kumar <charankumarbijjaram@gmail.com>
2025-07-06 18:07:29 +0530

    Mine Transactions and  The Blockchain and Cryptocurrency Backend

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const PubSub = require('./app/pubsub');
 const request = require('request'); // Using request for HTTP requests
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
+
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool,wallet });
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -39,9 +42,13 @@ app.post('/api/transact', (req, res) => {
     let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey });
     try {
         if (transaction) {
-            transaction.update({ senderWallet: wallet, recipient, amount });
+          transaction.update({ senderWallet: wallet, recipient, amount });
         } else {
-            transaction = wallet.createTransaction({ recipient, amount });
+          transaction = wallet.createTransaction({
+            recipient,
+            amount,
+            chain: blockchain.chain
+          });
         }
     } catch(error) {
         return res.status(400).json({ type: 'error', message: error.message });
@@ -53,6 +60,21 @@ app.post('/api/transact', (req, res) => {
 
 app.get('/api/transaction-pool-map', (req, res) => {
     res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+  transactionMiner.mineTransactions();
+
+  res.redirect('/api/blocks');
+});
+
+app.get('/api/wallet-info', (req, res) => {
+  const address = wallet.publicKey;
+
+  res.json({
+    address,
+    balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
+  });
 });
 
 
